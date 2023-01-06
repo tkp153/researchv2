fc655508333b730f633b8a419a6bf5344f96b195
tkp153 <takap365best@gmail.com>
2023-01-06 14:06:39 +0900

    backup

---
+++ build/slam_toolbox/slam_toolbox__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/researchv2/build/slam_toolbox/slam_toolbox__py
+# Build directory: /root/researchv2/build/slam_toolbox/slam_toolbox__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
