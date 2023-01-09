2033c575c2c8b20f5cb658a6307361469b270497
tkp153 <takap365best@gmail.com>
2023-01-09 12:27:29 +0900

    launch 実行をパス指定にしなくてもできるように改良実施

---
+++ install/chaser_system/share/chaser_system/first_navigation.launch.py
@@ -0,0 +1,49 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+
+    package_dir = get_package_share_directory("chaser_system")
+    #urdf = os.path.join(package_dir, "urdf" , "first_robot.urdf")
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    #world_path = os.path.join(package_dir, 'world', 'first_robot.sdf')
+    map_path = os.path.join(package_dir, "map", 'hayashi_15_com.yaml')
+    nav_params_path = os.path.join(package_dir, "config", 'first_navigation.yaml')
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
+    #rviz = os.path.join(package_dir, "rviz" , "first_robot.rviz")
+    rviz = os.path.join(package_dir, "rviz" , "nav2_view.rviz")
+    ekf_path = os.path.join(package_dir, "config", 'first_ekf.yaml')
+
+
+    return LaunchDescription([
+
+        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
+                                             description='Flag to enable joint_state_publisher_gui'),
+
+        
+
+
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_path,
+                'use_sim_time': use_sim_time,
+                'params_file': nav_params_path}.items(),
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz],
+            parameters=[{'use_sim_time': use_sim_time}],
+            output='screen'),
+        
+    ])
--- src/chaser_system/setup.py
+++ src/chaser_system/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*_launch.py')),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
         (os.path.join('share', package_name, 'config'), glob('config/*')),
